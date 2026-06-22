345b8c7fe8c27020502b54083e93d5169311fdfa
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2026-06-22 15:52:52 +0700

    pose subscriber

---
+++ ros2_gazebo_samples/ros2_ws/src/my_robot_controller/my_robot_controller/pose_subscriber.py
@@ -0,0 +1,22 @@
+
+import rclpy
+from rclpy.node import Node
+from turtlesim.msg import Pose 
+
+class PoseSubscriberNode(Node):
+    def __init__(self):
+        super().__init__('pose_subscriber')
+        self.pose_subscriber = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
+
+    def pose_callback(self, pose_msg: Pose):
+        self.get_logger().info(f'pose_callback: {pose_msg}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PoseSubscriberNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
