90f625792017d8ac18e41b1d1d454edae72301d5
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2026-06-23 14:46:55 +0700

    add service

--- ros2_gazebo_samples/ros2_ws/src/my_robot_controller/my_robot_controller/turtle_controller.py
+++ ros2_gazebo_samples/ros2_ws/src/my_robot_controller/my_robot_controller/turtle_controller.py
@@ -2,6 +2,8 @@ import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose 
 from geometry_msgs.msg import Twist
+from turtlesim.srv import SetPen
+from functools import partial
 
 class TurtleControllerNode(Node):
     def __init__(self):
@@ -21,6 +23,38 @@ class TurtleControllerNode(Node):
             cmd.angular.z = 0.0
         self.cmd_vel_pub.publish(cmd)
         self.get_logger().info(f'Published velocity command to move the turtle.')
+
+        if pose_msg.x > 5.0:
+            self.call_set_pen_service(0, 0, 255, 3, 0)
+        elif pose_msg.x < 2.0:
+            self.call_set_pen_service(255, 0, 0, 3, 0)
+        elif pose_msg.y > 5.0:
+            self.call_set_pen_service(0, 255, 0, 3, 0)
+        elif pose_msg.y < 2.0:
+            self.call_set_pen_service(0, 0, 255, 3, 0) 
+        else:
+            self.call_set_pen_service(0, 255, 255, 3, 0) 
+
+    def call_set_pen_service(self, r, g, b, width, off):
+        client = self.create_client(SetPen, '/turtle1/set_pen')
+        while not client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+        request = SetPen.Request()
+        request.r = r
+        request.g = g
+        request.b = b
+        request.width = width
+        request.off = off
+        future = client.call_async(request)
+        future.add_done_callback(self.callback_set_pen)
+    
+    def callback_set_pen(self, future):
+        try:
+            response = future.result()
+        except Exception as e:
+            self.get_logger().error(f'service callback failed with exception: {e}')
+            return
+        self.get_logger().info(f'Set pen response: {response}')
 
 def main(args=None):
     rclpy.init(args=args)
